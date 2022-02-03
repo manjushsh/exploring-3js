f0dfb3b5ff452e8ef448c0b9e5174718e30f0cfc
manjushsh <manjushsh@protonmail.com>
2022-02-03 10:08:11 +0530

    something renders

--- src/components/load-3d-object/index.jsx
+++ src/components/load-3d-object/index.jsx
@@ -3,31 +3,31 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'; // for gLTF 3d object loading
 
 // Import model assets
-import gLTF from './assets/landscape/scene.gltf';
+import cheeseBurgerFLB from './assets/burger/cheeseburger.glb';
 
 const Main3DSceneView = () => {
-    const sceneParentNode = React.createRef();
+    const sceneParentNode = React.createRef(); // as React.RefObject<HTMLDivElement>;
     useEffect(() => LandscapeModel3D(sceneParentNode));
     return (
         <div ref={sceneParentNode} className='3d-node'></div>
     );
 };
 
-const LandscapeModel3D = (parentNodeRef) => {
+const LandscapeModel3D = (parentNodeRef) => {   // : React.RefObject<HTMLDivElement>
     const renderer = new THREE.WebGLRenderer();
     const loader = new GLTFLoader();
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 0, 10);
+    // camera.position.set(0, 0, 10);
     scene.background = new THREE.Color(0xdddddd);
-    loader.load(gLTF, function (gltf) {
+    loader.load(cheeseBurgerFLB, gltf => {
         // renderer.setSize(window.innerWidth, window.innerHeight);
         // use ref as a mount point of the Three.js scene instead of the document.body
-        const canvasNode = parentNodeRef.current;
-        canvasNode?.appendChild(renderer.domElement);
         scene.add(gltf.scene);
         renderer.render(scene, camera);
-    }, undefined, function (error) {
+        const canvasNode = parentNodeRef.current;
+        canvasNode?.appendChild(renderer.domElement);
+    }, undefined, error => {
         console.error(error);
     });
 }
