d46b78e38b696d0c58b15e9821de3c76eccc0257
manjushsh <manjushsh@protonmail.com>
2022-02-27 15:36:43 +0530

    LED Added

--- src/components/load-3d-object/canvas-mapping.tsx
+++ src/components/load-3d-object/canvas-mapping.tsx
@@ -2,12 +2,14 @@ import FlowerCanvas from "./flowers";
 import FreeCarCanvas from "./freecar";
 import LightCanvas from "./light";
 import HeadSetCanvas from "./headphone";
+import TVCanvas from "./LED";
 
 const canvasMapping = {
   LightCanvas,
   FlowerCanvas,
   FreeCarCanvas,
   HeadSetCanvas,
+  TVCanvas,
 };
 
 export default canvasMapping;
